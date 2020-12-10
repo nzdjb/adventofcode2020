eb07865f52541d1d8f5ca2a3487183fdffa3eb15
Dave <dave@2a1b.com>
2020-12-10 10:04:25 +0000

    feat: Day 10 part 2 soln.

--- puzzles/day10/main.go
+++ puzzles/day10/main.go
@@ -30,6 +30,27 @@ func part1(adaptors []int) {
 	fmt.Println("Ones:", ones, "Threes:", threes, "Product:", ones*threes)
 }
 
+func part2(adaptors []int) {
+	adaptors = append([]int{0}, adaptors...)
+	paths := []int{1}
+	for i := range adaptors {
+		n, j, iplus, x := 0, 1, i + 1, 0
+		if iplus >= len(adaptors) {
+			x = adaptors[len(adaptors) - 1] + 3
+		} else {
+			x = adaptors[iplus]
+		}
+		for j <= 3 {
+			if iplus - j >= 0 && x - adaptors[iplus - j] <= 3 {
+				n += paths[iplus - j]
+			}
+			j++
+		}
+		paths = append(paths, n)
+	}
+	fmt.Println(paths[len(paths) - 1])
+}
+
 func main() {
 	lines := util.ScanFileToStringSlice("./input.txt")
 	adaptors := []int{}
@@ -39,4 +60,5 @@ func main() {
 	}
 	sort.Ints(adaptors)
 	part1(adaptors)
+	part2(adaptors)
 }
