1dd065d847305c8b5c3911774ec487cdc33635cf
Dave <dave@2a1b.com>
2020-12-13 07:45:28 +0000

    feat: Day12 part 2 soln.

--- puzzles/day12/main.go
+++ puzzles/day12/main.go
@@ -55,16 +55,79 @@ func enactInstruction(instruction string, quantity, x, y, bearing int) (int, int
 	return newX, newY, newBearing
 }
 
+func enactInstruction2(instruction string, quantity, shipX, shipY, wayX, wayY int) (int, int, int, int) {
+	newShipX, newShipY, newWayX, newWayY := shipX, shipY, wayX, wayY
+	switch instruction {
+	case "N":
+		newWayY = wayY + quantity
+	case "S":
+		newWayY = wayY- quantity
+	case "E":
+		newWayX = wayX + quantity
+	case "W":
+		newWayX = wayX - quantity
+	case "L":
+		switch quantity {
+		case 90:
+			newWayX = wayY * -1
+			newWayY = wayX
+		case 180:
+			newWayX = wayX * -1
+			newWayY = wayY * -1
+		case 270:
+			newWayX = wayY
+			newWayY = wayX * -1
+		default:
+			panic(fmt.Errorf("bad angle %v", quantity))
+		}
+	case "R":
+		switch quantity {
+		case 90:
+			newWayX = wayY
+			newWayY = wayX * -1
+		case 180:
+			newWayX = wayX * -1
+			newWayY = wayY * -1
+		case 270:
+			newWayX = wayY * -1
+			newWayY = wayX
+		default:
+			panic(fmt.Errorf("bad angle %v", quantity))
+		}
+	case "F":
+		newShipX = shipX + wayX*quantity
+		newShipY = shipY + wayY*quantity
+	default:
+		panic(fmt.Errorf("bad instruction"))
+	}
+	fmt.Println(newShipX, newShipY, newWayX, newWayY)
+	return newShipX, newShipY, newWayX, newWayY
+}
+
 func manhattan(x, y, dx, dy int) int {
 	return util.Abs(dx) - util.Abs(x) + util.Abs(dy) - util.Abs(y)
 }
 
-func main() {
-	lines := util.ScanFileToStringSlice("./input.txt")
+func part1(lines []string) {
 	x, y, bearing := 0, 0, 90
 	for _, line := range lines {
 		instruction, quantity := parseInstruction((line))
 		x, y, bearing = enactInstruction(instruction, quantity, x, y, bearing)
 	}
-	fmt.Println(manhattan(0, 0, x, y))
+	fmt.Println("part1:", manhattan(0, 0, x, y))
+}
+
+func part2(lines []string) {
+	shipX, shipY, wayX, wayY := 0, 0, 10, 1
+	for _, line := range lines {
+		instruction, quantity := parseInstruction((line))
+		shipX, shipY, wayX, wayY = enactInstruction2(instruction, quantity, shipX, shipY, wayX, wayY)
+	}
+	fmt.Println("part2:", manhattan(0, 0, shipX, shipY))
+}
+
+func main() {
+	lines := util.ScanFileToStringSlice("./input.txt")
+	part1(lines)
+	part2(lines)
 }
